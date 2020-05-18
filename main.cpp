c45980e6edf22697f0166d933095bd8c7929e0c5
batuhangunday <batuhan.gunday@icloud.com>
2020-05-18 19:08:12 +0300

    doesn't work ...

--- main.cpp
+++ main.cpp
@@ -4,9 +4,19 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
-    RedBlackTree<int> rbt;
-    rbt.insert(34);
+    //RedBlackTree<int> *rbt;
+    //rbt->insert(34);
+    //rbt->insert(23);
+    //rbt->insert(12);
+    //rbt->insert(43);
+    //rbt->print();
+
+    RedBlackTree<int> rbt = RedBlackTree<int>();
+
     rbt.insert(23);
+    rbt.insert(22);
+    rbt.insert(25);
+    rbt.insert(27);
     rbt.print();
 
     return 0;
